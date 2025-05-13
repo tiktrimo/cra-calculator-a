3fe9107d05bfb956a16bc451d45f8c48746f4039
suyong <suyong.lee@samsung.com>
2025-05-13 10:56:40 +0900

    getGop구현

--- calc.cpp
+++ calc.cpp
@@ -3,12 +3,21 @@
 class Cal
 {
 public:
-
+	int getGop(int a, int b)
+	{
+		return a * b;
+	}
 };
 
 TEST(t1, t2)
 {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, Gop)
+{
+	Cal calc;
+	EXPECT_EQ(12,calc.getGop(3,4));
 }
 
 int main()
