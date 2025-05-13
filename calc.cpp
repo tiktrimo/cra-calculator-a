6c495aae908972fa7a86e5f6a9ad401dc2157a91
seoyeong <syeong1121@gmail.com>
2025-05-13 11:05:06 +0900

    add getSum funtion and TC

--- calc.cpp
+++ calc.cpp
@@ -3,12 +3,21 @@
 class Cal
 {
 public:
+	int getSum(int a, int b) {
+		return a + b;
+	}
 
 };
 
 TEST(t1, t2)
 {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, SumTC) {
+	Cal cal;
+
+	EXPECT_EQ(8, cal.getSum(3, 5));
 }
 
 int main()
