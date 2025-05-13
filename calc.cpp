3217d971590c3dbece0c3a99280598772bf6264e
email <tiktrimo@gmail.com>
2025-05-13 10:56:12 +0900

    Add GetZegop and Test code

--- calc.cpp
+++ calc.cpp
@@ -3,12 +3,22 @@
 class Cal
 {
 public:
-
+	int getZegop(int a)
+	{
+		return a * a;
+	}
 };
 
 TEST(t1, t2)
 {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(Calculator, Zegop)
+{
+	Cal cal;
+	int result = cal.getZegop(5);
+	EXPECT_EQ(25, result);
 }
 
 int main()
