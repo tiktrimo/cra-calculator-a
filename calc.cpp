f57af10a4e586d85a3eb9d485056c3b263617a71
Sowon <dsdon9.lee@samsung.com>
2025-05-13 11:01:47 +0900

    getSumSum 추가

--- calc.cpp
+++ calc.cpp
@@ -3,12 +3,20 @@
 class Cal
 {
 public:
+	int getSumSum(int a, int b, int c) {
+		return a + b + c;
+	}
 
 };
 
 TEST(t1, t2)
 {
 	EXPECT_EQ(1, 1);
+}
+TEST(getSumSum, t1) {
+	Cal c;
+
+	EXPECT_EQ(10, c.getSumSum(2, 3, 5));
 }
 
 int main()
